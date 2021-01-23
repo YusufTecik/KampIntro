53e12571187da3f122c4468849ab948f07904a5c
yusuf tecik <tecikyusuf732@gmail.com>
2021-01-23 23:51:39 +0200

    23 Ocak DERS SONU

---
+++ OOP_2/TuzelMusteri.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_2
+{
+    class TuzelMusteri : Musteri
+    {
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
---
+++ OOP_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasıtKrediManeger = new TasıtKrediManeger();
+            IKrediManager konutKredianager = new KonutKredianager();
+
+            ILoggerService dataBaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager,fileLoggerService);
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasıtKrediManeger };
+
+            //basvuruManager.KrdiOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
